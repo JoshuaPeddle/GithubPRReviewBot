0c5a3809f233dd81e066bf30dbd514b5ca2fa9c7
Joshua Peddle - dev <Joshuapeddle128@gmail.com>
2024-08-19 22:14:43 -0230

    First Commit

---
+++ GithubPRReviewBot/Models/GitHubWebhookPayload.cs
@@ -0,0 +1,31 @@
+﻿namespace GithubPRReviewBot.Models
+{
+    public class GitHubWebhookPayload
+    {
+        public string Action { get; set; }
+        public Issue Issue { get; set; }
+        public Comment Comment { get; set; }
+        public Repository Repository { get; set; }
+    }
+
+    public class Issue
+    {
+        public int Number { get; set; }
+    }
+
+    public class Comment
+    {
+        public string Body { get; set; }
+    }
+
+    public class Repository
+    {
+        public string Name { get; set; }
+        public Owner Owner { get; set; }
+    }
+
+    public class Owner
+    {
+        public string Login { get; set; }
+    }
+}
